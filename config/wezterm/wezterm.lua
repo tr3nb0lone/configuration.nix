477e26702dade77aa5802f2f4f9f366503893de5
tr3nb0lone <tr3nacetate@proton.me>
2025-11-13 14:59:41 +0300

    feat: wezterm FTW.

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+local wezterm = require 'wezterm'
+
+local config = {
+	colors = {
+		cursor_bg = "white",
+		cursor_border = "white",
+	},
+
+	font_size = 12.5,
+	font = wezterm.font ('Google Sans Code', { weight = 'Medium' }),
+
+	-- color_scheme = 'Belge (terminal.sexy)',
+
+	enable_tab_bar = false,
+	window_padding = { left = 0, right = 0,	top = 0, bottom = 0, },
+	window_background_opacity = 0.899,
+
+	cursor_thickness = "1px",
+}
+
+return config
